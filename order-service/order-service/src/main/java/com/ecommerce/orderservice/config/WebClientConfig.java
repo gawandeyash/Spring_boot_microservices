d3f026e35c0f4debb7f695e308f1f8af0628bd44
gawandeyash <imyashng@gmail.com>
2024-04-08 10:47:06 +0530

    Implemented inventory service

---
+++ order-service/order-service/src/main/java/com/ecommerce/orderservice/config/WebClientConfig.java
@@ -0,0 +1,10 @@
+package com.ecommerce.orderservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebClientConfig {
+    @Bean
+    public WebClient
+}
